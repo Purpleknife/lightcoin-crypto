8723fcadfb80a7f441686b602e3167b2ec7469f4
Hind <hind.nayhi@gmail.com>
2022-08-05 13:54:04 -0600

    Added features to the LightCoin App using OOP logic.

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let sum = 0;
+    for (let money of this.transactions) {
+      sum += money.value;
+    }
+    return sum;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+  commit() {
+    if (!this.isAllowed()) {
+      return false;
+    }
+
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  isAllowed() {
+    return true;
+  }
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
 
 
+  get value() {
+    return -this.amount;
+  }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+  isAllowed() {
+    return ((this.amount.balance - this.amount) >= 0);
+  }
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+}
+
+const myAccount = new Account('PurpleKnife');
+
+console.log('Current balance before transactions is:', myAccount.balance);
+
+const t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log(`Oops, you tried to withdraw. Balance is ${myAccount.balance}, we cannot proceed with this transaction.`);
 
-console.log('Balance:', balance);
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+console.log('Deposit:', myAccount.balance);
+
+console.log('Current balance after transactions is:', myAccount.balance);
+console.log('Your transaction history is:', myAccount.transactions);
